6c1aebf08a2daa05eec39bce71579fc2a079a11a
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2025-11-21 19:26:46 -0300

    Quienes somos

--- src/components/Home/Somos/QuienesSomos.jsx
+++ src/components/Home/Somos/QuienesSomos.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react";
 
 const QuienesSomos = () => {
   return (
-    <div>QuienesSomos</div>
-  )
-}
+    <section className="w-full py-16 flex justify-center bg-[var(--color-background-forth)]">
 
-export default QuienesSomos
+      {/* Contenedor */}
+      <div className="w-[95%] max-w-6xl">
+
+        {/* Título */}
+        <h2 className="text-[45px] md:text-4xl font-bold text-center mb-12 text-[var(--color-titulos)]">
+          ¿QUIENES SOMOS?
+        </h2>
+
+        {/* Contenido en dos columnas */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
+
+          {/* Imagen */}
+          <div className="flex justify-center">
+            <img
+              src="/quienes.png"
+              alt="Nosotros"
+              className="w-64 h-64 object-cover rounded-full shadow-lg"
+            />
+          </div>
+
+          {/* Texto */}
+          <div className="leading-relaxed text-lg bg-[var(--recuadro)] p-5">
+            <p className="text-[var(--p-negro)] text-[20px]">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+              Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+              Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
+              massa. Vestibulum lacinia arcu eget nulla.
+            </p>
+          </div>
+
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default QuienesSomos;
--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -3,8 +3,11 @@ export const themeColors = {
         '--color-background': '#ffffff',
         '--color-background-secondary': '#EBECEC',
         '--color-background-third': '#242424',
+        '--color-background-forth': '#F1F1F3',
         '--color-titulos': '#C94800',
         '--p-blanco': '#ffffff',
+        '--p-negro': '#000000',
+        '--recuadro': '#F6F5F5',
     },
 
     dark: {
