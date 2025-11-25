686830d0d3dc71723c67fffdd71f063ebc72d4ef
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2025-11-25 19:16:07 -0300

    Quienes somos

--- src/components/Home/Somos/QuienesSomos.jsx
+++ src/components/Home/Somos/QuienesSomos.jsx
@@ -13,7 +13,7 @@ const QuienesSomos = () => {
         </h2>
 
         {/* Contenido en dos columnas */}
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 items-center">
 
           {/* Imagen */}
           <div className="flex justify-center">
@@ -26,13 +26,13 @@ const QuienesSomos = () => {
 
           {/* Texto */}
           <div className="leading-relaxed text-lg bg-[var(--recuadro)] p-5">
-            <p className="text-[var(--p-negro)] text-[20px]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
-              Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
-              Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
-              massa. Vestibulum lacinia arcu eget nulla.
+            <p className="text-[var(--p-negro)] text-[20px] mb-4">
+           <span className="text-[var(--color-titulos)]">Jonil Gougeon</span>: Apasionado deportista, emprendedor de Juan Lacaze con una destacada trayectoria como triatleta e Ironman finisher, ha demostrado su determinación y dedicación en cada desafío. Como director de Mundo Running TV y organizador de eventos deportivos desde 2015, está dejando una huella imborrable en el mundo deportivo.
             </p>
+
+             <p className="text-[var(--p-negro)] text-[20px]">
+              <span className="text-[var(--color-titulos)]">Natalia Hernández</span>: Profesional destacada que se desempeña como codirectora y fotógrafa en Mundo Running TV. Asimismo, como deportista aficiona combina su pasión por el deporte con su compromiso empresarial.
+             </p>
           </div>
 
         </div>
