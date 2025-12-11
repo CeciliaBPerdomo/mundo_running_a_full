5e18214ac85a7a990585ede6fb35dd1e3bfaac05
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2025-12-11 16:58:24 -0300

    render.yaml

--- src/axios/registro-axios.js
+++ src/axios/registro-axios.js
@@ -4,6 +4,7 @@ export const crearUsuario = async (nombre, email, password, celular, ciudad) => 
     try {
         const ruta = import.meta.env.VITE_RUTA
         const url = ruta + "auth/register"
+        
         const user = { nombre, email, password, celular, ciudad }
 
         const response = await axios.post(url, user)
