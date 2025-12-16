0c51c20a20dc0971374f1733d82c48fd0bb09dec
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2025-12-16 15:19:23 -0300

    Mail con brevo

--- backend/mailer/transporter.ts
+++ backend/mailer/transporter.ts
@@ -1,30 +1,53 @@
 import nodemailer from "nodemailer"
 import 'dotenv/config';
 
+// export const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.GMAILEMAIL,
+//     pass: process.env.GMAILPASS
+//   },
+//   from: 'cecilia.perdomo@gmail.com',
+
+//   // CONFIGURACIÓN CRÍTICA PARA RENDER
+//   host: 'smtp.gmail.com',
+//   port: 587,
+//   secure: false, // true para 465, false para 587
+//   requireTLS: true,
+//   tls: { ciphers: 'SSLv3', rejectUnauthorized: false },
+//   connectionTimeout: 10000,
+//   greetingTimeout: 10000,
+//   socketTimeout: 10000,
+//   debug: true
+// })
+
+// transporter.verify((error, success) => {
+//   if (error) {
+//     console.error("Error en transporter:", error);
+//   } else {
+//     console.log("Transporter listo para enviar mails ✔️");
+//   }
+// });
+
 export const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.GMAILEMAIL,
-    pass: process.env.GMAILPASS
-  },
-  from: 'cecilia.perdomo@gmail.com',
-
-  // CONFIGURACIÓN CRÍTICA PARA RENDER
-  host: 'smtp.gmail.com',
+  host: 'smtp-relay.brevo.com',
   port: 587,
-  secure: false, // true para 465, false para 587
-  requireTLS: true,
-  tls: { ciphers: 'SSLv3', rejectUnauthorized: false },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-  debug: true
-})
+  secure: false,
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS
+  }
+});
 
+// Función para probar la conexión inmediatamente
 transporter.verify((error, success) => {
   if (error) {
-    console.error("Error en transporter:", error);
+    console.error("❌ Error de autenticación SMTP:", {
+      error: error.message,
+    //  code: error.code,
+      usuario: process.env.BREVO_USER
+    });
   } else {
-    console.log("Transporter listo para enviar mails ✔️");
+    console.log("✅ SMTP configurado correctamente para:", process.env.BREVO_USER);
   }
 });
