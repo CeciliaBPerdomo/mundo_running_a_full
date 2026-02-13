d9a1c6c036247bd599b010cc141032725b04a683
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2026-02-13 19:03:10 -0300

    Compras del cliente

--- src/components/UI/IconButton/IconButton.jsx
+++ src/components/UI/IconButton/IconButton.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const IconButton = ({ onClick, children, ariaLabel, className = "" }) => {
     return (
-        <button onClick={onClick} aria-label={ariaLabel} className={`flex items-center px-2.5 py-2 bg-[var(--color-background-black)] hover:bg-[var(--color-titulos)] transition ${className}`}>
+        <button 
+        onClick={onClick} 
+        aria-label={ariaLabel} 
+        className={`flex items-center px-2.5 py-2 bg-[var(--color-background-black)] hover:bg-[var(--color-titulos)] transition ${className}`}>
             {children}
         </button>
     );
--- src/components/Usuario/Compras.jsx
+++ src/components/Usuario/Compras.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { obtenerHistorialCarritos } from "../../axios/carrito-axios";
 import { cantidadProductos } from "../../helpers/compras/compras";
+import { Link } from "react-router";
 
 
 const MisCompras = () => {
@@ -37,52 +38,66 @@ const MisCompras = () => {
 
     return (
         <>
-            <h1 className="text-3xl font-bold text-[var(--color-titulos)] mb-6 text-center">
-                Tus compras
-            </h1>
-            <div className="overflow-x-auto bg-white rounded-2xl shadow border">
-                <table className="min-w-full text-sm">
-                    <thead className="bg-[var(--color-background-third)] text-[var(--p-blanco)]">
-                        <tr>
-                            <th className="px-4 py-3 text-left">Fecha pedido</th>
-                            <th className="px-4 py-3 text-center">Cant. de productos</th>
-                            <th className="px-4 py-3 text-left">Dirección de envío</th>
-                            <th className="px-4 py-3 text-right">Total</th>
-                            <th className="px-4 py-3 text-center">Estado</th>
-                        </tr>
-                    </thead>
+            {compras.length > 0 ?
+                <div>
+                    <h1 className="text-3xl font-bold text-[var(--color-titulos)] mb-6 text-center">
+                        Tus compras
+                    </h1>
+                    <div className="overflow-x-auto bg-white rounded-2xl shadow border">
+                        <table className="min-w-full text-sm">
+                            <thead className="bg-[var(--color-background-third)] text-[var(--p-blanco)]">
+                                <tr>
+                                    <th className="px-4 py-3 text-left">Fecha pedido</th>
+                                    <th className="px-4 py-3 text-center">Cant. de productos</th>
+                                    <th className="px-4 py-3 text-left">Dirección de envío</th>
+                                    <th className="px-4 py-3 text-right">Total</th>
+                                    <th className="px-4 py-3 text-center">Estado</th>
+                                </tr>
+                            </thead>
 
-                    <tbody>
-                        {compras.map((carrito) => (
-                            <tr
-                                key={carrito._id}
-                                className="border-t hover:bg-gray-50 transition"
-                            >
-                                <td className="px-4 py-3">
-                                    {new Date(carrito.createdAt).toLocaleDateString()}
-                                </td>
+                            <tbody>
+                                {compras.map((carrito) => (
+                                    <tr
+                                        key={carrito._id}
+                                        className="border-t hover:bg-gray-50 transition"
+                                    >
+                                        <td className="px-4 py-3">
+                                            {new Date(carrito.createdAt).toLocaleDateString()}
+                                        </td>
 
-                                <td className="px-4 py-3 text-center">
-                                    {cantidadProductos(carrito.items)}
-                                </td>
+                                        <td className="px-4 py-3 text-center">
+                                            {cantidadProductos(carrito.items)}
+                                        </td>
 
 
-                                <td className="px-4 py-3">
-                                    {carrito.envio?.direccion || "—"}, {carrito.envio?.ciudad || "—"}
-                                </td>
+                                        <td className="px-4 py-3">
+                                            {carrito.envio?.direccion || "—"}, {carrito.envio?.ciudad || "—"}
+                                        </td>
 
-                                <td className="px-4 py-3 text-right font-semibold">
-                                    u$s {carrito.total}
-                                </td>
+                                        <td className="px-4 py-3 text-right font-semibold">
+                                            u$s {carrito.total}
+                                        </td>
 
-                                <td className={`px-4 py-3 text-center font-semibold ${estadoColor[carrito.estado]}`}>
-                                    {carrito.estado}
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+                                        <td className={`px-4 py-3 text-center font-semibold ${estadoColor[carrito.estado]}`}>
+                                            {carrito.estado}
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                :
+                <div>
+                    <p className="text-center">Aún no haz realizado ninguna compra.</p>
+                    <p className="text-center">¿Qué estás esperando?</p>
+                    <div className="flex justify-center mt-5">
+                        <Link to="/tienda_deportiva">
+                            <button className="flex items-center gap-2 px-4 py-2 rounded-md bg-[var(--botones-rojos)] text-[var(--p-blanco)] hover:bg-[var(--botones-rojos-hover)] transition">Visitar tienda</button>
+                        </Link>
+                    </div>
+                </div>
+            }
         </>
     );
 };
